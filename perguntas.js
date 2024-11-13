26c6ad8ac0afe46e2390485bba58c5ca81ee7976
Maria Teresa <maria.santos.carvalho14@escola.pr.gov.br>
2024-11-13 15:52:49 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,45 @@
+criaCartao(
+    'Química', 
+    'O que é um átomo?', 
+    'É a menor unidade de matéria que mantém as propriedades de um elemento químico.'
+)
+criaCartao(
+    'Química', 
+    'O que são prótons, nêutrons e elétrons?',
+    'São partículas elementares onde os prótons possuem carga positiva, os nêutrons não possuem carga e os elétrons possuem carga negativa.'
+)
+criaCartao(
+    'Química', 
+    'O que é um elemento químico?', 
+    'Uma substância composta por átomos com o mesmo número de prótons.'
+)
+criaCartao(
+    'Química', 
+    'O que é uma molécula?', 
+    'É um conjunto de dois ou mais átomos unidos por ligações químicas.'
+)
+criaCartao(
+    'Química', 
+    'O que é uma ligação iônica?', 
+    'É uma ligação que ocorre pela transferência de elétrons entre átomos, formando íons.'
+)
+criaCartao(
+    'Química', 
+    'O que é uma ligação covalente?', 
+    'É a ligação em que dois átomos compartilham pares de elétrons.'
+)
+criaCartao(
+    'Química', 
+    'O que é uma reação química?', 
+    'É um processo no qual matéria (reagentes) se transforma em novas substâncias (produtos).'
+)
+criaCartao(
+    'Química',
+    'O que é uma solução?', 
+    'É uma mistura aquosa homogênea de duas ou mais substâncias, onde uma delas é o solvente (geralmente em maior quantidade) a outra é o soluto.'
+)
+criaCartao(
+    'Química', 
+    'O que é pH?', 
+    'É uma medida de acidez ou basicidade de uma solução, variando de 0 (ácido) a 14 (básico).'
+)
